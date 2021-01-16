5013615e4ca2a378e4304abca84e29250d49b5e9
Kaan Yilmaz <douscriptist@gmail.com>
2021-01-17 00:25:59 +0300

    ts setup

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from "components/Layout";
+
+const About = () => {
+  return (
+    <Layout title="About">
+      <h1>About me.</h1>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "components/Layout";
+
+export default function Home() {
+  return (
+    <Layout title="Welcome">
+      <h1>Hello, friend.</h1>
+    </Layout>
+  );
+}
