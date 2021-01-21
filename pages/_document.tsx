27b4f3f05e0688c4f33c090c04eb70a489fc6a8d
Kaan Yilmaz <douscriptist@gmail.com>
2021-01-21 04:35:34 +0300

    future metadata usage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,13 @@ class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
+        <Head>
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100&family=Montserrat:wght@100&display=swap"
+            rel="stylesheet"
+          /> */}
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
