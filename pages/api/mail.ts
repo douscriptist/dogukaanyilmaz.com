66f164d53650c8943e3d33ef4152940a51ff2231
Kaan Yilmaz <douscriptist@gmail.com>
2021-01-23 23:55:34 +0300

    add nodemailer

---
+++ pages/api/mail.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { sendEmail } from "utils/sendMail";
+
+// type Data = {
+//   name: string;
+// };
+
+export default async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  if (req.method === "POST") {
+    // Process a POST request
+    try {
+      await sendEmail({
+        senderMail: req.body.email,
+        subject: "Access to info",
+        name: req.body.name,
+        message: req.body.message,
+      });
+      res.status(200).json({
+        success: true,
+        message: "Email sent.",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ success: false, message: "Bir hata olustu, lutfen daha sonra tekrar deneyiniz" });
+    }
+  } else if (req.method === "GET") {
+    res.status(200).json({ success: true, message: "Mail url, GET" });
+  } else {
+    res.status(500).json({ success: false, message: "Endpoint not provided!" });
+  }
+};
