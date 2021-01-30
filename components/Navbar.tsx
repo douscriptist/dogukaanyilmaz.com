07ef08adc4c12beb90f4f8bbee6c05c6d9843aba
Kaan Yilmaz <douscriptist@gmail.com>
2021-01-30 20:13:08 +0300

    fix dynamic routes & icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,51 +2,48 @@ import { Box, Button, HStack, Tag, TagLabel, useColorMode } from "@chakra-ui/rea
 import Link from "next/link";
 import { useRouter } from "next/router";
 import useLocale from "hooks/useLocale";
-import { EmailIcon, LinkIcon, SpinnerIcon, StarIcon } from "@chakra-ui/icons";
+import { EmailIcon, LinkIcon, SpinnerIcon, StarIcon, SettingsIcon } from "@chakra-ui/icons";
+import { useEffect } from "react";
 
-const RouteIcon = (route: string) => {
-  let Icon;
-  if (route === "home") {
-    Icon = EmailIcon;
-  } else if (route === "about") {
-    Icon = SpinnerIcon;
-  } else if (route === "theming") {
-    Icon = StarIcon;
-  } else if (route === "contact") {
-    Icon = EmailIcon;
-  } else {
-    Icon = LinkIcon;
-  }
-  return <Icon mr={2} />;
-};
+const IconEnum = {
+  home: EmailIcon,
+  about: SpinnerIcon,
+  theming: StarIcon,
+  contact: LinkIcon,
+  // apps: SettingsIcon,
+} as const;
 
 const Navbar = () => {
   const { pathname } = useRouter();
   const { t } = useLocale();
   const { colorMode, toggleColorMode, setColorMode } = useColorMode();
 
+  useEffect(() => {}, []);
+
   return (
     <nav>
       <Box d="flex" justifyContent="center">
         <HStack spacing={4}>
-          {["home", "about", "theming", "contact"].map((route, i) => (
-            <Link href={`/${route === "home" ? "" : route}`} key={i}>
-              <a>
-                <Tag
-                  mt={1}
-                  size="md"
-                  variant="outline"
-                  colorScheme="teal"
-                  color={pathname === `/${route === "home" ? "" : route}` ? "gray.200" : "inherit"}
-                  bgColor={pathname === `/${route === "home" ? "" : route}` ? "teal.500" : "inherit"}
-                  _hover={{ cursor: "pointer", background: "teal.800", color: "white" }}
-                >
-                  {RouteIcon(route)}
-                  <TagLabel>{t(`${route}`)}</TagLabel>
-                </Tag>
-              </a>
-            </Link>
-          ))}
+          {Object.entries(IconEnum).map(([route, Icon]) => {
+            return (
+              <Link href={`/${route === "home" ? "" : route}`} key={route}>
+                <a>
+                  <Tag
+                    mt={1}
+                    size="md"
+                    variant="outline"
+                    colorScheme="teal"
+                    color={pathname === `/${route === "home" ? "" : route}` ? "gray.200" : "inherit"}
+                    bgColor={pathname === `/${route === "home" ? "" : route}` ? "teal.500" : "inherit"}
+                    _hover={{ cursor: "pointer", background: "teal.800", color: "white" }}
+                  >
+                    <Icon mr={2} />
+                    <TagLabel>{t(`${route}`)}</TagLabel>
+                  </Tag>
+                </a>
+              </Link>
+            );
+          })}
         </HStack>
       </Box>
       <span style={{ position: "absolute", right: 10, top: "50%" }}>
