9e9e0f4a4c7916ce7563bf94c2e182cffaef36ef
Kaan Yilmaz <douscriptist@gmail.com>
2021-02-01 04:20:35 +0300

    apps & utility

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -1,32 +1,87 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Divider, Fade, Image, Kbd, Stack, StackDivider, VStack } from "@chakra-ui/react";
 import Layout from "components/Layout";
 import useLocale from "hooks/useLocale";
-import { useEffect } from "react";
-import { getAllApps } from "utils/getApps";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { getAllApps, DouApp, DouApps } from "utils/getApps";
+import { getRandomColor } from "utils/themeUtils";
 
-interface Props {}
+interface AppsProps {
+  apps: {
+    done: DouApp[];
+    undone: DouApp[] | [];
+  };
+}
 
-const Apps = ({ apps }: any) => {
+const Apps = ({ apps }: AppsProps) => {
   const { t } = useLocale();
-  useEffect(() => {
-    console.log("effect", apps);
-  }, []);
+
   return (
     <Layout pageTitle={t("apps")}>
-      <Box d="flex" justifyContent="center" alignItems="center" h="95vh"></Box>
+      <Box d="flex" flexDir="column" justifyContent="center" alignItems="center" h="95vh">
+        {apps.done.map((app: DouApp) => {
+          // setTimeout(() => {
+          const { bg, color } = getRandomColor();
+          return (
+            <Fade in key={app.name}>
+              <Box
+                d="flex"
+                flexDir="row"
+                px="10px"
+                py="10px"
+                mt="4"
+                bg={bg}
+                color={color}
+                rounded="md"
+                shadow="md"
+                w={500}
+              >
+                <Box>
+                  <Image
+                    boxSize="140px"
+                    objectFit="cover"
+                    src={`/images/${app.name}.png`}
+                    alt={app.url}
+                    borderRadius="0.375em"
+                  />
+                </Box>
+                <Stack direction="row" my="auto" h="8em" px={5}>
+                  <Divider orientation="vertical" borderColor={color === "white" ? "gray.200" : "gray.700"} />
+                </Stack>
+                <Box my="auto">
+                  <VStack
+                    divider={<StackDivider borderColor={color === "white" ? "gray.200" : "gray.700"} />}
+                    spacing={4}
+                    align="baseline"
+                  >
+                    <Kbd color={color} bg={bg} borderColor={color === "white" ? "gray.200" : "gray.700"}>
+                      {app.name}
+                    </Kbd>
+                    <Link href={`https://${app.url}/`}>
+                      <a target="_blank" rel="noreferrer">
+                        {app.url}
+                      </a>
+                    </Link>
+                  </VStack>
+                </Box>
+              </Box>
+            </Fade>
+          );
+          // }, 1000);
+        })}
+      </Box>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
   const apps = await getAllApps();
+
+  // const apps {}
   return {
     props: {
       apps,
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every second
     revalidate: 60 * 60 * 24, // In seconds
   };
 }
---
+++ utils/themeUtils.ts
@@ -0,0 +1,12 @@
+export const colors = ["red", "teal", "green", "blue", "orange", "yellow", "cyan", "purple", "pink"];
+export const degrees = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+export const getRandomColor = () => {
+  const c = Math.floor(Math.random() * colors.length);
+  const d = Math.floor(Math.random() * degrees.length);
+
+  const bg = `${colors[c]}.${degrees[d]}`;
+  const color = d < degrees.indexOf(400) ? "black" : "white";
+  console.log(bg, color, "ddsad");
+  return { bg, color };
+};
